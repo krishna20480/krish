dbf60af9f038407e7e9eb31cb5d7a2a6dcdc4aeb
krishna20480 <superstarkrishna@atomicmail.io>
2025-05-15 09:30:38 +0000

    Update bro.sh

--- bro.sh
+++ bro.sh
@@ -4,10 +4,10 @@
 apt update 
 
 # Install Hydra and Nmap
-apt install -y hydra nmap
+apt install -y hydra
 
 # Install required Python modules
-pip3 install nmap requests
+pip3 install requests
 
 # Run your scripts
 python3 rz.py
